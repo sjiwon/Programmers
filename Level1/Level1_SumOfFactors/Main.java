6034ac621afc1a4cce0bf98491865ddb805e7be7
SeoJiWon <sjiwon4491@gmail.com>
2022-07-10 18:48:28 +0900

    [Level 1] 약수의 합

---
+++ Level1/Level1_SumOfFactors/Main.java
@@ -0,0 +1,14 @@
+package Level1.Level1_SumOfFactors;
+// Level 1 : 약수의 합
+
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+
+        for(int i=1; i<=n; i++){
+            if(n%i == 0)
+                sum += i;
+        }
+        return sum;
+    }
+}
