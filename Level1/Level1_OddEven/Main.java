5b24d6cc6b8e8ab3919cd3d44d0fb4c69cfbe64e
SeoJiWon <sjiwon4491@gmail.com>
2022-07-10 18:46:20 +0900

    [Level 1] 짝수와 홀수

---
+++ Level1/Level1_OddEven/Main.java
@@ -0,0 +1,17 @@
+package Level1.Level1_OddEven;
+// Level 1 : 짝수와 홀수
+
+class Solution {
+    public String solution(int num) {
+        if(num%2 == 0)
+            return "Even";
+        else
+            return "Odd";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
