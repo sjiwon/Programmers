a7415a0ba5fecaab2e1aa7568bfd2bdedd68df1a
SeoJiWon <sjiwon4491@gmail.com>
2022-07-10 18:48:14 +0900

    [Level 1] 문자열을 정수로 바꾸기

---
+++ Level1/Level1_StringToNum/Main.java
@@ -0,0 +1,16 @@
+package Level1.Level1_StringToNum;
+// Level 1 : 문자열을 정수로 바꾸기
+
+class Solution {
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println("1234");
+        System.out.println("-1234");
+    }
+}
